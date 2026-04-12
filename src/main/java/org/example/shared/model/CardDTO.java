62506c32f496799175e59f55b464d20744ab53ec
Andreapolimi <vaccai.andrea@gmail.com>
2026-04-12 08:58:14 +0200

    Removed usefull interface Renderable.

--- src/main/java/org/example/shared/model/CardDTO.java
+++ src/main/java/org/example/shared/model/CardDTO.java
@@ -2,4 +2,4 @@ package org.example.shared.model;
 
 import java.io.Serializable;
 
-public record CardDTO(String id) implements Serializable, Renderable {}
+public record CardDTO(String id) implements Serializable {}
