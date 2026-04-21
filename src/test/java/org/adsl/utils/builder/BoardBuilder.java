6b13020259474d897eb2ea85b55c9b667c2e4e2f
Andreapolimi <vaccai.andrea@gmail.com>
2026-04-21 16:24:36 +0200

    Solved bug in fillTopRow and tested behaviour.

--- src/test/java/org/adsl/utils/builder/BoardBuilder.java
+++ src/test/java/org/adsl/utils/builder/BoardBuilder.java
@@ -2,34 +2,48 @@ package org.adsl.utils.builder;
 
 import org.adsl.server.model.board.*;
 import org.adsl.server.model.cards.Card;
-import org.adsl.utils.fakes.FakeCardRow;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
-
 public class BoardBuilder {
-    public CardRow lowRow = new FakeCardRow();
-    public CardRow topRow = new FakeCardRow();
+    public CardRow lowRow = null;
+    public CardRow topRow = null;
     public OfferTrack offerTrack;
     public OrderTile orderTile;
     public ArrayList<Set<Card>> remainingBuildings = new ArrayList<>();
     public Deck deck = new Deck();
 
+    public static class Param {
+        public int numCardTopRow;
+        public int numTribeCardTopRow;
+        public int numCardLowRow;
+        public int numTribeCardLowRow;
 
-    public Board build() {
-        return new Board(lowRow, topRow, offerTrack, orderTile,
-                remainingBuildings, deck);
+        public Param(int numCardTopRow, int numTribeCardTopRow, int numCardLowRow, int numTribeCardLowRow) {
+            this.numCardTopRow = numCardTopRow;
+            this.numTribeCardTopRow = numTribeCardTopRow;
+            this.numCardLowRow = numCardLowRow;
+            this.numTribeCardLowRow = numTribeCardLowRow;
+        }
     }
 
-    public BoardBuilder withTopRow(CardRow cardRow) {
-        topRow = cardRow;
-        return this;
+    public Map<Integer, Param> parameters;
+
+    public BoardBuilder() {
+        this.parameters = new HashMap<>();
+        parameters.put(2, new Param(9, 6, 6, 3));
+        parameters.put(3, new Param(11, 7, 8, 4));
+        parameters.put(4, new Param(12, 8, 9, 5));
+        parameters.put(5, new Param(14, 9, 11, 6));
     }
 
-    public BoardBuilder withLowRow(CardRow cardRow) {
-        lowRow = cardRow;
-        return this;
+    public Board build(int numPlayer) {
+        Param param = parameters.get(numPlayer);
+        return new Board(param.numCardLowRow, param.numTribeCardLowRow,
+                param.numCardTopRow, param.numTribeCardTopRow,
+                offerTrack, orderTile, deck);
     }
-
 }
