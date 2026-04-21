88461c3fdeadb571e7549065588f960be9ae99cb
Andreapolimi <vaccai.andrea@gmail.com>
2026-04-21 15:06:07 +0200

    Further update.

---
+++ src/test/java/org/adsl/utils/fakes/FakeCard.java
@@ -0,0 +1,26 @@
+package org.adsl.utils.fakes;
+
+import org.adsl.server.model.Player;
+import org.adsl.server.model.cards.Card;
+import org.adsl.shared.enums.CardType;
+import org.adsl.shared.enums.Trigger;
+
+import java.util.Map;
+import java.util.Set;
+
+public class FakeCard extends Card {
+    public FakeCard() {
+        super("id", 1, 5);
+    }
+
+    @Override
+    public boolean canBeDrawn(Player p) {
+        return true;
+    }
+
+    @Override
+    public void insert(Map<CardType, Set<Card>> cards) {}
+
+    @Override
+    public void activeEffect(Set<Player> players, Trigger t) {}
+}
