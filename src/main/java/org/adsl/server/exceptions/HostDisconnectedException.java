3baec92deafb836631e87333991ae35bc17f725c
Andreapolimi <vaccai.andrea@gmail.com>
2026-05-07 10:51:57 +0200

    Handle hostDisconnection. Handle only host can start a game.

---
+++ src/main/java/org/adsl/server/exceptions/HostDisconnectedException.java
@@ -0,0 +1,7 @@
+package org.adsl.server.exceptions;
+
+public class HostDisconnectedException extends ServerException {
+    public HostDisconnectedException(String message) {
+        super(message);
+    }
+}
