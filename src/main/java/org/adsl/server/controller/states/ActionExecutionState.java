274c0568ba52856b8fc48840dc478580eba40da3
Samu <samuelewu04@gmail.com>
2026-05-11 17:01:40 +0200

    GUI Picked Card Detail & Quick Start Tool Fix
    Chat log now also shows which card(s) each player has picked instead of just saying how many.
    
    Added executable bit to quick start tool so no chmod needed anymore and pudated README.
    
    Closes #45

--- src/main/java/org/adsl/server/controller/states/ActionExecutionState.java
+++ src/main/java/org/adsl/server/controller/states/ActionExecutionState.java
@@ -14,6 +14,8 @@ import org.adsl.shared.enums.Trigger;
 import org.adsl.server.model.Game;
 import org.adsl.server.model.Player;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
@@ -97,6 +99,7 @@ public class ActionExecutionState extends ControllerState {
   }
 
   private void execute(Set<Move> moves, Player p) {
+    List<String> pickedNames = new ArrayList<>();
     for (Move move : moves) {
       CardRow selectedRow;
       if (move.row() == Row.UPPER) {
@@ -105,12 +108,14 @@ public class ActionExecutionState extends ControllerState {
         selectedRow = getGame().getBoard().lowRow();
       }
       Card selectedCard = selectedRow.pickCardAt(move.rowIndex());
+      pickedNames.add(selectedCard.getClass().getSimpleName());
       selectedCard.insert(p.getCards());
     }
     OfferTrack offerTrack = getGame().getBoard().offerTrack();
     offerTrack.removePlayer(p);
     placeTotem(p);
-    String log = "[ACTION] Player " + p.getName() + " picked " + moves.size() + " card(s).";
+    String log = "[ACTION] Player " + p.getName() + " picked " + moves.size()
+            + " card(s): " + String.join(", ", pickedNames) + ".";
     System.out.println(log);
     setNextState(calcNextState());
     getGame().sendUpdateGame(log);
